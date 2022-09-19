5446f2fb05c49f6f652c0797c3fc2502574bbc8e
elliotabrahamsson <elliotsson@outlook.com>
2022-09-19 12:12:46 +0200

    tries for loops

--- exercises.js
+++ exercises.js
@@ -1,5 +1,14 @@
 console.log('I am alive!');
 
 function exerciseA() {
+    var textboxvalue = document.getElementById('txt').value;
+    const siffror = textboxvalue.split (' ');
+
+    for (let i = 0; i < siffror.length; i++) {
+        if (siffror[i] ===theValue) {
+            break;
+        }
+        console.log(siffror);
+    } 
     
 }
