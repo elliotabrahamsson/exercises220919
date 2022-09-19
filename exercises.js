d4eef7621d6e889259c3b5eacd197c8fd73cf0c7
Elliot <elliotsson@outlook.com>
2022-09-19 18:26:31 +0200

    the array writes out everything

--- exercises.js
+++ exercises.js
@@ -1,8 +1,10 @@
 function exerciseA() {
     var textboxvalue = document.getElementById('txt').value;
-    var input = textboxvalue.split(' ');
     const siffror = ['2', '3', '5', '7', '11', '13', '17', '19'];
 
-    console.log(siffror.indexOf(input));
+    for (let i = 0; i < siffror.length; i++) {
+        console.log(`${siffror[i]}`);
+        
+    }
            
 }
