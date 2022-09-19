73e29f3296b167bdcf8bc69517f8fcef6069c58c
Elliot <elliotsson@outlook.com>
2022-09-19 19:08:37 +0200

    got a prime funtion that sees if its a prime number

--- exercises.js
+++ exercises.js
@@ -1,10 +1,22 @@
 function exerciseA() {
     var textboxvalue = document.getElementById('txt').value;
-    const siffror = ['2', '3', '5', '7', '11', '13', '17', '19'];
+    let start = 0;
 
-    for (let i = 0; i < siffror.length; i++) {
-        console.log(`${siffror[i]}`);
-        
+    function prime(siffror, start) {
+        while (start <= Math.sqrt(siffror)) {
+            if (siffror % start++ < 1) {
+                return false;
+            }
+        }
+        return siffror > 1;
     }
-           
+   
+
+    /*for (let i = 0; i < siffror.length; i++) {
+        const bignumber = (element) => element > textboxvalue;
+        //console.log(siffror.find(bignumber));
+        console.log(`${siffror.find[i]}`);
+        
+    }*/
+    console.log([2, 3, 5, 7, 11, 13].findIndex(prime));    
 }
